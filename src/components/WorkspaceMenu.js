254f964fb9b3dc78a6f790558af1926734426bb0
Seth Randall <sethjrandall@gmail.com>
2022-01-06 12:43:05 -0500

    [FIXED] WorkspaceMenu showing up on /workspaces

--- src/components/WorkspaceMenu.js
+++ src/components/WorkspaceMenu.js
@@ -4,10 +4,16 @@ import {
   faAlignJustify,
   faTasks,
 } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 
 const WorkspaceMenu = () => {
+  const location = useLocation();
   return (
-    <section className="hidden lg:block w-1/5 bg-gray-200 rounded-lg m-3 p-4 overflow-hidden shadow-lg h-fit">
+    <section
+      className={`${
+        location.pathname === "/workspaces" ? "lg:hidden" : "lg:block"
+      } hidden w-1/5 bg-gray-200 rounded-lg m-3 p-4 overflow-hidden shadow-lg h-fit`}
+    >
       <h1 className="text-2xl text-primary">Workspace Title</h1>
       <h2 className="text-sm my-2">Workspace Description goes here</h2>
       <hr className="border-primary mx-auto my-4" />
